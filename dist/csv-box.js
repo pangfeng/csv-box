a898b766f5925ccc8e86d72cab563eac1fba71ba
pangfeng001 <pangfeng001@ke.com>
2018-11-07 15:45:09 +0800

    upgrade regexp

--- dist/csv-box.js
+++ dist/csv-box.js
@@ -1,2 +1,178 @@
-var cbox=function(e){var t={};function r(n){if(t[n])return t[n].exports;var o=t[n]={i:n,l:!1,exports:{}};return e[n].call(o.exports,o,o.exports,r),o.l=!0,o.exports}return r.m=e,r.c=t,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(e,t){if(1&t&&(e=r(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)r.d(n,o,function(t){return e[t]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=0)}([function(e,t,r){"use strict";r.r(t),r.d(t,"csv",function(){return u});let n=/,(?=([^,"]+,)*("([^"]|"{2})+"|[^",]+$))/g,o=e=>{let t=e.split("\r\n"),r=t.shift().split(",").map(e=>e.trim()),o=[];return t.slice(0).forEach(e=>{let t=e.replace(n,"$__arsdff__$").replace(/^"|"$/g,"").replace(/"{2}/g,'"').split(/"?\$__arsdff__\$"?/),u={};r.forEach((e,r)=>{u[e]=t[r]}),o.push(u)}),o};const u=(e,t)=>{if(!e||"function"!=typeof t)return null;if("string"==typeof e){let r=e,n=new XMLHttpRequest;n.open("GET",r,!1),n.onreadystatechange=function(){200!==n.status&&0!=n.status||t(o(n.responseText))},n.send()}else if("object"==typeof e){let r=new FileReader;r.readAsText(e),r.onload=function(e){t(o(this.result))}}}}]);
+var cbox =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! exports provided: csv, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "csv", function() { return csv; });
+let regExp = /,(?=,*(([^,"]|"{2})+(,|$))*(("([^"]|"{2})+")|$))/g;
+// let regExp = /,(?=([^,"]+,)*("([^"]|"{2})+"|[^",]+$))/g;
+
+/**
+ * 
+ * cell中出现逗号时会有双引号包围：
+ *  "([^"]|"{2})+",
+ *  没逗号的：
+ *  ([^,"]|"{2})+,
+ * 没有逗号的0-n个加一个有逗号的或是$:
+ * '323,dw,sdf,w""er,"a,a,a",111,"22,22",555,"3""3,33"",""33"'.replace(/,(?=((([^,"]|"{2})+,)*("([^"]|"{2})+"(,|$))|([^,"]|"{2})+$))/g,'-')
+ * '"ss,ee,",323,dw,sdf,w""er,"a,a,a",111,"22,22",555,"3""3,33"",""33",444'.replace(/,(?=((([^,"]|"{2})+,)*("([^"]|"{2})+"(,|$))|([^,"]|"{2})+$))/g,'-')
+ * 
+ * 不包含逗号的0-n个加上包含逗号的或是$:
+ * '323,dw,sdf,w""er,"a,a,a",111,"22,22",555,"3""3,33"",""33"'.replace(/,(?=(([^,"]|"{2})+(,|$))*(("([^"]|"{2})+")|$))/g,'-')
+ * '"ss,ee,",323,dw,sdf,w""er,"a,a,a",111,"22,22",555,"3""3,33"",""33",444'.replace(/,(?=(([^,"]|"{2})+(,|$))*(("([^"]|"{2})+")|$))/g,'-')
+ * 
+ * 
+ */
+
+
+let parse = str =>{
+    let list = str.split("\r\n"),
+        header = list.shift().split(',').map(s=>s.trim()),
+        body = list.slice(0),
+        results = [];
+    
+
+    body.forEach(element => {
+        let item = element.replace(regExp,'$__arsdff__$').replace(/^"|"$/g,"").replace(/"{2}/g,"\"").split(/"?\$__arsdff__\$"?/);
+        let line = {};
+
+        header.forEach((o, i)=>{
+            line[o] = item[i];
+        });
+
+        results.push(line)
+    });
+
+    return results;
+}
+
+const csv = (input, callback)=>{
+    if(!input || typeof callback !== 'function'){
+        return null;
+    }
+
+    if(typeof input === 'string'){
+        let file = input,
+            rawFile = new XMLHttpRequest()
+        ; 
+
+        rawFile.open("GET", file, false); 
+
+        rawFile.onreadystatechange = function() { 
+            if(rawFile.status === 200 || rawFile.status == 0){
+                callback(parse(rawFile.responseText));
+            }
+        }; 
+
+        rawFile.send(); 
+    }else if(typeof input === 'object'){
+        let reader = new FileReader();
+
+        reader.readAsText(input);
+        
+        reader.onload = function(val){
+            callback(parse(this.result));
+        }
+    }
+}
+
+
+/* harmony default export */ __webpack_exports__["default"] = (csv);
+
+/***/ })
+
+/******/ });
 //# sourceMappingURL=csv-box.js.map
